130c7d9e7d4bf91e4df2aaff37392bd4d7146104
Fabio Göldi <Fabio Gldi>
2017-03-21 13:30:35 +0100

    node.js dateien

---
+++ pw.js
@@ -0,0 +1,12 @@
+const PASSWORD = "IUK123";
+const USERNAME = "root";
+const CAMERAIP = "192.168.0.90";
+const PORT = 3000;
+
+
+module.exports = {
+    PASSWORD: PASSWORD,
+    USERNAME: USERNAME,
+    CAMERAIP: CAMERAIP,
+    PORT: PORT
+};
