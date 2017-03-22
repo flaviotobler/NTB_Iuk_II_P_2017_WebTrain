4ad3e926c6d1793966f664e5770a8258885832ff
Fabio Göldi <fabgoeldi@gmail.com>
2017-03-22 15:57:30 +0100

    Code dokumentation erweitert

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 //benötigte Module laden
+/*pw.js einbinden, in diesem File sind passwort und ip adresse der Netzwerkkamera hinterlegt
+zusätzlich auch der PORT für den server*/
 const PW = require('./pw.js');
+
+/*verwendete module werden eingebunden*/
 var express = require('express');
 var app = express();
 var request = require('request');
 var MjpegProxy = require('mjpeg-proxy').MjpegProxy;
 
+/*über das hinterlegte passwort und die IP Adresse wird die Url um der Kamera Befehle zu schicken gebildet */
 var baseUrl = 'http://'+ PW.USERNAME +':'+ PW.PASSWORD +'@'+ PW.CAMERAIP +'';
 
+/*dateien im Ordner 'public' werden als statische web dateien zur verfügung gestellt
+public/ ist das root verzeichnis für die webseite, bei aufruf des nodejs servers über
+einen Browser wird public/index.html angezeigt	*/
 app.use(express.static('public'));
 
+/* http POST request werden vom node.js server verarbeitet in den flgenden express funktionen
+wenn ein POST request die jeweilige URL ausgeführt wird, z.B. http://URLNODEJS/up wird die dazugehörige Funktion in app.post('/up, function(){} ausgeführt  */
 app.post('/up', function (req, res) {
 	console.log("Button Up pressed");
   	camAction("move=up");
@@ -54,6 +64,7 @@ app.get('/video.mjpg', new MjpegProxy(baseUrl +'/mjpg/video.mjpg').proxyRequest)
 app.listen(PW.PORT, function () {
   console.log('Example app listening on port 3000!');
 });
+
 
 function camAction(param){
     request(baseUrl +'/axis-cgi/com/ptz.cgi?' + param + '', function(error){
