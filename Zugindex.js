6e831a8fce806fd9be7e708bcd1a161b29283f01
flaviotobler <flavio.to@hispeed.ch>
2017-03-29 16:16:33 +0000

    Add files via upload

--- Zugindex.js
+++ Zugindex.js
@@ -8,26 +8,28 @@ var state = 0;
 //GPIO initialisieren
 pfio.init();
 
-/*Wird der Button start gedrückt auf der Homepage sendet dieser ein /start, mit einem wert in diesem Fall eine 0,1 (0->port, 1-> status)
-Mit console.log wird auf dem Terminal der String Button Start pressed ausgegen und der Port auf high gesetz
-Die Variable state wird von 0 auf 1 gesetzt damit der Benutzer auf der Website den jetztigen Status des Zuges sieht
+/*
+Wird der Button start gedrückt auf der Homepage sendet dieser ein /start, mit einem wert in diesem Fall eine 0,1 (0->port, 1-> status).
 */
-app.post('/start', function (req, res) {
-	console.log("Button Start pressed");
-	pfio.digital_write(0, 1);
-	state=1;
-	res.send('POST request to homepage');
-});
+app.post('/start', function (req, res)
+		{
+			console.log("Button Start pressed"); //Debug-Meldung ausgeben
+			pfio.digital_write(0, 1); //Zug starten
+			state=1; //Zustand abspeichern (benoetigt bei der Abfrage)
+			res.status(200).send(""+state); //Server-Request beantworten
+		}
+	);
 
-/*Wird der Button stop gedrückt auf der Homepage geschehen dieselben Schritte wie beim Drücken auf Button start
+/*
+Wird der Button stop gedrückt auf der Homepage geschehen dieselben Schritte wie beim Drücken auf Button start
 In diesem ändert die Variable state auf 0 und der Port wird auf low gesetzt
 */
 
 app.post('/stop', function (req, res) {
  	console.log("Button Stop pressed");
-	pfio.digital_write(0, 1);
+	pfio.digital_write(0, 0);
 	state=0;
-	res.send('POST request to homepage');
+	res.status(200).send(""+state);
 });
 
 //Wird der Server gestartet mittel node Zugindex wird die HTML Seite Zugindex.html aufgerufen
@@ -39,4 +41,11 @@ app.post('/stop', function (req, res) {
 //Die Seite ist auf dem Port 3000 aufzurufen und zeigt dies auf dem Terminal
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+});
+
+app.post('/getState', function (req, res) {
+ 	console.log("Button getState pressed: " + state);
+	
+	res.status(200).send(""+state);
+
 });
