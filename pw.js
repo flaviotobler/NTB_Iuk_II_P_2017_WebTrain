cc21a2521030916e43e510def29b7ae0536c7b60
fabgeo8 <fabgoeldi@gmail.com>
2017-04-04 13:26:56 +0200

    pw.js dokumentiert

--- pw.js
+++ pw.js
@@ -1,3 +1,9 @@
+/*
+Modul, das vom index.js eingebunden wird.
+Hier sind alle nötigen Daten für die Verbindung zur Kamera und andere Angaben wie nodejs Serverport eingetragen.
+Wenn die Kamera ins NTB-Netzwerk verlegt wird, müssen hier nur noch die angaben angepasst werden
+*/
+
 const PASSWORD = "IUK123";
 const USERNAME = "root";
 const CAMERAIP = "192.168.0.90";
